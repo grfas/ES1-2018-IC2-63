4fe584dfa54ecf3d9bc2ca710cbddc804915baa6
jgdre-iscteiulpt <jgdre@iscte-iul.pt>
2018-10-16 17:45:50 +0100

    Alterações no main de modo a abrir a interface gráfica.

--- Project/src/main.java
+++ Project/src/main.java
@@ -1,12 +1,31 @@
 import Facebook.InicializadorFacebook;
 
-public class main {
-	
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/application/GUI.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("MyTitle");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] arg0){
+		// TODO Auto-generated method stub
 		InicializadorFacebook iniciaFace = new InicializadorFacebook();
 		
 		iniciaFace.IniciaFace();
+		launch(arg0);
+		
+		
 	}
 
-
+	
 }
