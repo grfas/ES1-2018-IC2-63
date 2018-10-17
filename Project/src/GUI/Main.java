fd22bfde32e192a89e7ecc93c5cee11c6ebe18a9
jgdre-iscteiulpt <jgdre@iscte-iul.pt>
2018-10-17 13:42:41 +0100

    dafegq

---
+++ Project/src/GUI/Main.java
@@ -0,0 +1,31 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/GUI/GUI.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("MyTitle");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] arg0){
+		// TODO Auto-generated method stub
+		//InicializadorFacebook iniciaFace = new InicializadorFacebook();
+		
+		//iniciaFace.IniciaFace();
+		launch(arg0);
+		
+		
+	}
+
+	
+}
