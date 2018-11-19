a5dc238eb246bd8a6f8ef5b67208f7e165b91342
xico <lbfjo@iscte-iul.pt>
2018-11-19 18:14:10 +0000

    Facebook Dependencies

--- Project/src/Facebook/CustomUser.java
+++ Project/src/Facebook/CustomUser.java
@@ -1,75 +1,75 @@
-//package Facebook;
-//
-//
-//import java.util.Date;
-//
-//import com.restfb.Facebook;
-//import com.restfb.types.Post;
-//
-//
-//
-//public class CustomUser {
-//	
-//	@Facebook("post")
-//	private Post post;
-//	
-//	@Facebook("first_name")
-//	private String firstName;
-//
-//	@Facebook("last_name")
-//	private String lastName;
-//
-//	@Facebook("name")
-//	private String fullName;
-//
-//	@Facebook("email")
-//	private String email;
-//
-//	@Facebook("birthday")
-//	private Date birthday;
-//
-//	public String getFirstName() {
-//		return firstName;
-//	}
-//
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//
-//	public String getLastName() {
-//		return lastName;
-//	}
-//
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
-//
-//	public String getFullName() {
-//		return fullName;
-//	}
-//
-//	public void setFullName(String fullName) {
-//		this.fullName = fullName;
-//	}
-//
-//	public String getEmail() {
-//		return email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	public Date getBirthday() {
-//		return birthday;
-//
-//	}
-//
-//	public void setBirthday(Date birthday) {
-//		this.birthday = birthday;
-//	}
-//	public Post getPost() {
-//		
-//		return post;
-//	}
-//}
+package Facebook;
+
+
+import java.util.Date;
+
+import com.restfb.Facebook;
+import com.restfb.types.Post;
+
+
+
+public class CustomUser {
+	
+	@Facebook("post")
+	private Post post;
+	
+	@Facebook("first_name")
+	private String firstName;
+
+	@Facebook("last_name")
+	private String lastName;
+
+	@Facebook("name")
+	private String fullName;
+
+	@Facebook("email")
+	private String email;
+
+	@Facebook("birthday")
+	private Date birthday;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public Post getPost() {
+		
+		return post;
+	}
+}
