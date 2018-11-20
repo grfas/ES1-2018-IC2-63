f094e7721c75ac0b534f9fc55bb287a39cefaba6
jgdre-iscteiulpt <jgdre@iscte-iul.pt>
2018-11-20 18:04:05 +0000

    Email implementado

--- Project/src/Email/EmailReader.java
+++ Project/src/Email/EmailReader.java
@@ -1,8 +1,9 @@
 package Email;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
- 
+
 import javax.mail.Address;
 import javax.mail.BodyPart;
 import javax.mail.Folder;
@@ -13,96 +14,120 @@ import javax.mail.internet.MimeMultipart;
 
 import GUI.Gui;
 import twitter4j.Status;
- 
+
 /**
-* Class reads emails
-*
-* @author outlook
-*
-*/
+ * Class reads emails
+ *
+ * @author outlook
+ *
+ */
 
 public class EmailReader {
 	public List<MessagePrint> MensagemDoIscte = new ArrayList<MessagePrint>();
-	
+	private String filtroEmail="ISCTE-IUL";
+
+	public String getFiltroEmail() {
+		return filtroEmail;
+	}
+
+	public void setFiltroEmail(String filtroEmail) {
+		this.filtroEmail = filtroEmail;
+	}
+
 	public List<MessagePrint> getMensagemDoIscte() {
 		return MensagemDoIscte;
 	}
+
 //	public Gui gui;
 //	public EmailReader(Gui gui) {
 //		this.gui=gui;
 //	}
-  
- 
-   /**
-    * Method reads emails from the IMAP or POP3 server.
-    * @param isImap - if true then we are reading messages from the IMAP server, if no then read from the POP3 server.
-    */
-   public  void readEmails(boolean isImap) {
-       // Create all the needed properties - empty!
-       Properties connectionProperties = new Properties();
-       // Create the session
-       Session session = Session.getDefaultInstance(connectionProperties,null);
-      
-       try {
-           System.out.print("Connecting to the IMAP server...");
-           // Connecting to the server
-           // Set the store depending on the parameter flag value
-           String storeName = isImap ? "imaps" : "pop3";    
-           Store store = session.getStore(storeName);
-            
-           // Set the server depending on the parameter flag value
-           String server = isImap ? "imap.outlook.com" : "pop3.outlook.com";
-           store.connect(server,"%Mail%","%Pass%");
-            
-           System.out.println("done!");
-            
-           // Get the Inbox folder
-           Folder inbox = store.getFolder("Inbox");
-            
-           // Set the mode to the read-only mode
-           inbox.open(Folder.READ_ONLY);
-            
-           // Get messages
-           Message messages[] = inbox.getMessages();
-            
-           System.out.println("Reading messages...");
-            
-           // Display the messages
-           for(Message message:messages) {
-               for (Address a: message.getFrom()){
-            	   String b = a.toString();
-                  
-               
-               String result = b.split(" ")[0];
-               if(result.equals("ISCTE-IUL")) {
-               System.out.println("From:" +a);
-               System.out.println("Title: " + message.getSubject());
-              
-               System.out.println();
-               System.out.println(message.getContent());
-               Object content = message.getContent();
-               if (content instanceof MimeMultipart) {
-                   MimeMultipart multipart = (MimeMultipart) content;
-                   if (multipart.getCount() > 0) {
-                       BodyPart part = multipart.getBodyPart(0);
-                       content = part.getContent();
-                   }
-               }
-               if (content != null) {
+	public String user;
+	public String pass;
+
+	public EmailReader(String user, String pass) {
+		this.user = user;
+		this.pass = pass;
+	}
+
+	/**
+	 * Method reads emails from the IMAP or POP3 server.
+	 * 
+	 * @param isImap - if true then we are reading messages from the IMAP server, if
+	 *               no then read from the POP3 server.
+	 */
+	public void readEmails(boolean isImap) {
+		// Create all the needed properties - empty!
+		Properties connectionProperties = new Properties();
+		// Create the session
+		Session session = Session.getDefaultInstance(connectionProperties, null);
+
+		try {
+			System.out.print("Connecting to the IMAP server...");
+			// Connecting to the server
+			// Set the store depending on the parameter flag value
+			String storeName = isImap ? "imaps" : "pop3";
+			Store store = session.getStore(storeName);
+
+			// Set the server depending on the parameter flag value
+			String server = isImap ? "imap.outlook.com" : "pop3.outlook.com";
+			store.connect(server, user, pass);
+
+			System.out.println("done!");
+
+			// Get the Inbox folder
+			Folder inbox = store.getFolder("Inbox");
+
+			// Set the mode to the read-only mode
+			inbox.open(Folder.READ_ONLY);
+
+			// Get messages
+			Message messages[] = inbox.getMessages();
+
+			System.out.println("Reading messages...");
+
+			// Display the messages
+			int i = 0;
+			for (Message message : messages) {
+				for (Address a : message.getFrom()) {
+					String b = a.toString();
+					if (i < 50) {
+						i++;
+						String result = b.split(" ")[0];
+						if (result.equals(filtroEmail)) {
+							System.out.println("From:" + a);
+							System.out.println("Title: " + message.getSubject());
+
+							System.out.println();
+							System.out.println(message.getContent());
+							Object content = message.getContent();
+							if (content instanceof MimeMultipart) {
+								MimeMultipart multipart = (MimeMultipart) content;
+								if (multipart.getCount() > 0) {
+									BodyPart part = multipart.getBodyPart(0);
+									content = part.getContent();
+								}
+							}
+							if (content != null) {
 //                 System.out.println(content.toString());
-                 MessagePrint mp = new MessagePrint(a,message.getSubject(), content.toString());
-                 MensagemDoIscte.add(mp);
-                 System.out.println("lista emailReader"+ getMensagemDoIscte());
-                 
-               }
-               System.out.println("---");
-           }
-           }
-           }
-            
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-        
-       }
+								MessagePrint mp = new MessagePrint(a, message.getSubject(), content.toString());
+								MensagemDoIscte.add(mp);
+								System.out.println("lista emailReader" + getMensagemDoIscte());
+
+							}
+							System.out.println("---");
+						}
+					} else {
+						System.out.println("Não existem mais mensagens recentes");
+						return;
+					}
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- Project/src/GUI/Gui.java
+++ Project/src/GUI/Gui.java
@@ -20,6 +20,8 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JTabbedPane;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 import java.util.List;
@@ -42,51 +44,55 @@ public class Gui extends JFrame {
 	String selecao;
 	private List<Status> listaEstados;
 	JTextArea areaTweet = new JTextArea();
+	JTextArea areaEmail= new JTextArea();
 	private JButton btnActualizarF;
 	private JButton btnActualizarE;
 	private JButton btnAlterarFiltroE;
 	private JTextField textFieldF;
+	private JTextField textFieldE;
 	private static EmailReader email ;
-	
+
 	/**
 	 * Launch the application.
 	 * @throws TwitterException 
 	 */
 	public static void main(String[] args) {
-		
-		
-		
+
+
+
 		System.out.println("oioi");
 		System.out.println("14:39 - 20/11//2018");
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
+
 					Gui frame = new Gui();
 					frame.setVisible(true);
-//					frame.relistTweets();
+					//					frame.relistTweets();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
+
 		twitter= new TwitterApp();
-		email= new EmailReader();
+		String name = JOptionPane.showInputDialog("Introduza o user do email");
+		String pass = JOptionPane.showInputDialog("Introduza a PASS do user");
+		email= new EmailReader(name,pass);
 		try {
 			email.readEmails(true);
 			twitter.initTwitter();
-			
-			
+
+
 		} catch (TwitterException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
-		
-		
-		
+
+
+
 	}
 
 	/**
@@ -95,19 +101,20 @@ public class Gui extends JFrame {
 	public Gui() {
 		init();
 		events();
-		
+
 	}
-	
+
 	private void relistTweets() {
 
 		for(Status s : twitter.getListaTweets()){
 			model.addElement(s.getUser().getName()+ " - "+s.getCreatedAt());
 			System.out.println(s.getUser().getName()+ " - "+s.getCreatedAt());
 		}
-		
-		
+
+
 	}
 	
+
 	private void relistEmails() {
 
 		for(MessagePrint s : email.getMensagemDoIscte()){
@@ -115,58 +122,72 @@ public class Gui extends JFrame {
 			System.out.println(s.getFrom() + " - "+s.getTitulo());
 		}
 		
-		
+
+
 	}
 
 	private void events() {
-		
+
 		btnActualizarT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
 				model.clear();
 				for(Status s : twitter.getListaTweets()){
 					model.addElement(s.getUser().getName()+ " - "+s.getCreatedAt());
-					
+
 				}
-				
+
 				listaTweets = new JList<>(model);
 
 				System.out.println("button clicked!");
-				
+
 				areaTweet.setText(null);
-//				relistTweets();
-				
+				//				relistTweets();
+
 
 			}
 		});
-		
+
 		btnActualizarE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
 				modelEmail.clear();
 				System.out.println("lista email"+ email.getMensagemDoIscte());
-				
+
 				for(MessagePrint s : email.getMensagemDoIscte()){
 					modelEmail.addElement(s.getFrom() + " - "+s.getTitulo());
+
 					System.out.println(s.getFrom() + " - "+s.getTitulo());
 				}
-				
-				
+
+
 
 				System.out.println("button email clicked!");
-				
+
 				areaTweet.setText(null);
-//				relistTweets();
-				
+				//				relistTweets();
+
 
 			}
 		});
-		
+		btnAlterarFiltroE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				email.setFiltroEmail(textFieldE.getText());
+				email.readEmails(true);
+				System.out.println("filtro"+ email.getFiltroEmail());
+
+				System.out.println("button filtro email clicked!");
+
+
+			}
+		});
+
+
 		listaTweets.addListSelectionListener(new ListSelectionListener() {
-			
+
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-				
+
 				String selectedValue = (String) listaTweets.getSelectedValue();
 				System.out.println(selectedValue);
 				for(Status s : twitter.getListaTweets()) {
@@ -175,20 +196,39 @@ public class Gui extends JFrame {
 						areaTweet.setText(s.getText()); 	
 					}
 				}
-				
-				
-				
+
+
+
+			}
+		});
+
+		listaEmails.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				areaEmail.setText(null);
+				String selectedValue = (String) listaEmails.getSelectedValue();
+				System.out.println(selectedValue);
+				for(MessagePrint mp : email.getMensagemDoIscte()){
+					if(selectedValue.equals(mp.getFrom()+ " - "+mp.getTitulo())){
+						
+						areaEmail.setText(mp.getTexto()); 	
+					}
+				}
+
+
+
 			}
 		});
 	}
-	
-	
+
+
 	private void actualizarTweets() {
 		this.listaEstados.clear();
 		this.listaEstados=twitter.getListaTweets();
 	}
-	
-	
+
+
 
 	private void init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -196,171 +236,171 @@ public class Gui extends JFrame {
 		contentPane1 = new JPanel();
 		contentPane1.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane1);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout gl_contentPane1 = new GroupLayout(contentPane1);
 		gl_contentPane1.setHorizontalGroup(
-			gl_contentPane1.createParallelGroup(Alignment.LEADING)
+				gl_contentPane1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane1.createSequentialGroup()
-					.addGap(1)
-					.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 489, Short.MAX_VALUE))
-		);
+						.addGap(1)
+						.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 489, Short.MAX_VALUE))
+				);
 		gl_contentPane1.setVerticalGroup(
-			gl_contentPane1.createParallelGroup(Alignment.LEADING)
+				gl_contentPane1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane1.createSequentialGroup()
-					.addGap(5)
-					.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 336, Short.MAX_VALUE))
-		);
-		
+						.addGap(5)
+						.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 336, Short.MAX_VALUE))
+				);
+
 		JPanel panelEmail = new JPanel();
 		tabbedPane.addTab("Email", null, panelEmail, null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		
+
 		JScrollPane scrollPaneE = new JScrollPane();
-		
+
 		btnActualizarE = new JButton("Actualizar");
-		
-		JTextField textFieldE = new JTextField();
+
+		textFieldE = new JTextField();
 		textFieldE.setColumns(10);
-		
+
 		btnAlterarFiltroE = new JButton("Alterar Filtro");
 		GroupLayout gl_panelEmail = new GroupLayout(panelEmail);
 		gl_panelEmail.setHorizontalGroup(
-			gl_panelEmail.createParallelGroup(Alignment.LEADING)
+				gl_panelEmail.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panelEmail.createSequentialGroup()
-					.addGap(1)
-					.addComponent(scrollPaneE, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE)
-					.addGap(1)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelEmail.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnActualizarE)
-						.addComponent(textFieldE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAlterarFiltroE))
-					.addContainerGap(40, Short.MAX_VALUE))
-		);
+						.addGap(1)
+						.addComponent(scrollPaneE, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE)
+						.addGap(1)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panelEmail.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnActualizarE)
+								.addComponent(textFieldE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnAlterarFiltroE))
+						.addContainerGap(40, Short.MAX_VALUE))
+				);
 		gl_panelEmail.setVerticalGroup(
-			gl_panelEmail.createParallelGroup(Alignment.LEADING)
+				gl_panelEmail.createParallelGroup(Alignment.LEADING)
 				.addComponent(scrollPaneE, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 308, Short.MAX_VALUE)
 				.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 308, Short.MAX_VALUE)
 				.addGroup(gl_panelEmail.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnActualizarE)
-					.addGap(28)
-					.addComponent(textFieldE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnAlterarFiltroE)
-					.addContainerGap(192, Short.MAX_VALUE))
-		);
-		
-		
+						.addContainerGap()
+						.addComponent(btnActualizarE)
+						.addGap(28)
+						.addComponent(textFieldE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(btnAlterarFiltroE)
+						.addContainerGap(192, Short.MAX_VALUE))
+				);
+
+
 		scrollPaneE.setRowHeaderView(listaEmails);
+
 		
-		JTextArea areaEmails = new JTextArea();
-		scrollPane.setViewportView(areaEmails);
+		scrollPane.setViewportView(areaEmail);
 		panelEmail.setLayout(gl_panelEmail);
-		
+
 		JPanel panelFacebook = new JPanel();
 		tabbedPane.addTab("Facebook", null, panelFacebook, null);
-		
+
 		JScrollPane scrollPane_2 = new JScrollPane();
-		
+
 		JScrollPane scrollPane_3 = new JScrollPane();
-		
+
 		btnActualizarF = new JButton("Actualizar");
-		
+
 		textFieldF = new JTextField();
 		textFieldF.setColumns(10);
-		
+
 		JButton btnAlterarFiltroF = new JButton("Alterar Filtro");
 		GroupLayout gl_panelFacebook = new GroupLayout(panelFacebook);
 		gl_panelFacebook.setHorizontalGroup(
-			gl_panelFacebook.createParallelGroup(Alignment.LEADING)
+				gl_panelFacebook.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panelFacebook.createSequentialGroup()
-					.addGap(1)
-					.addComponent(scrollPane_2, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
-					.addGap(2)
-					.addComponent(scrollPane_3, GroupLayout.PREFERRED_SIZE, 203, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelFacebook.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnActualizarF)
-						.addComponent(textFieldF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAlterarFiltroF))
-					.addContainerGap(32, Short.MAX_VALUE))
-		);
+						.addGap(1)
+						.addComponent(scrollPane_2, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
+						.addGap(2)
+						.addComponent(scrollPane_3, GroupLayout.PREFERRED_SIZE, 203, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panelFacebook.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnActualizarF)
+								.addComponent(textFieldF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnAlterarFiltroF))
+						.addContainerGap(32, Short.MAX_VALUE))
+				);
 		gl_panelFacebook.setVerticalGroup(
-			gl_panelFacebook.createParallelGroup(Alignment.LEADING)
+				gl_panelFacebook.createParallelGroup(Alignment.LEADING)
 				.addComponent(scrollPane_3, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 308, Short.MAX_VALUE)
 				.addComponent(scrollPane_2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 308, Short.MAX_VALUE)
 				.addGroup(gl_panelFacebook.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnActualizarF)
-					.addGap(18)
-					.addComponent(textFieldF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnAlterarFiltroF)
-					.addContainerGap(202, Short.MAX_VALUE))
-		);
-		
+						.addContainerGap()
+						.addComponent(btnActualizarF)
+						.addGap(18)
+						.addComponent(textFieldF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(btnAlterarFiltroF)
+						.addContainerGap(202, Short.MAX_VALUE))
+				);
+
 		JTextArea areaPosts = new JTextArea();
 		scrollPane_3.setViewportView(areaPosts);
-		
+
 		JList listaPosts = new JList();
 		scrollPane_2.setViewportView(listaPosts);
 		panelFacebook.setLayout(gl_panelFacebook);
-		
+
 		JPanel panelTwitter = new JPanel();
 		tabbedPane.addTab("Twitter", null, panelTwitter, null);
-		
+
 		JScrollPane scrollPane_4 = new JScrollPane();
-		
+
 		JScrollPane scrollPane_5 = new JScrollPane();
-		
+
 		btnActualizarT = new JButton("Actualizar");
-		
+
 		JTextField txtFiltroT = new JTextField();
 		txtFiltroT.setText("Filtro");
 		txtFiltroT.setColumns(10);
-		
+
 		JButton btnAlterarFiltroT = new JButton("Alterar Filtro");
-		
+
 		GroupLayout gl_panelTwitter = new GroupLayout(panelTwitter);
 		gl_panelTwitter.setHorizontalGroup(
-			gl_panelTwitter.createParallelGroup(Alignment.LEADING)
+				gl_panelTwitter.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panelTwitter.createSequentialGroup()
-					.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
-					.addGap(1)
-					.addComponent(scrollPane_5, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelTwitter.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnActualizarT, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFiltroT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAlterarFiltroT))
-					.addContainerGap(44, Short.MAX_VALUE))
-		);
+						.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+						.addGap(1)
+						.addComponent(scrollPane_5, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panelTwitter.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnActualizarT, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtFiltroT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnAlterarFiltroT))
+						.addContainerGap(44, Short.MAX_VALUE))
+				);
 		gl_panelTwitter.setVerticalGroup(
-			gl_panelTwitter.createParallelGroup(Alignment.LEADING)
+				gl_panelTwitter.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panelTwitter.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnActualizarT)
-					.addGap(33)
-					.addComponent(txtFiltroT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnAlterarFiltroT)
-					.addContainerGap(192, Short.MAX_VALUE))
+						.addContainerGap()
+						.addComponent(btnActualizarT)
+						.addGap(33)
+						.addComponent(txtFiltroT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(btnAlterarFiltroT)
+						.addContainerGap(192, Short.MAX_VALUE))
 				.addGroup(gl_panelTwitter.createSequentialGroup()
-					.addGap(1)
-					.addComponent(scrollPane_4, GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE))
+						.addGap(1)
+						.addComponent(scrollPane_4, GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE))
 				.addGroup(gl_panelTwitter.createSequentialGroup()
-					.addGap(2)
-					.addComponent(scrollPane_5, GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
-		);
+						.addGap(2)
+						.addComponent(scrollPane_5, GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
+				);
+
+
 		
-		
-		scrollPaneE.setViewportView(areaEmails);
 		scrollPaneE.setViewportView(listaEmails);
-		
+
 		scrollPane_5.setViewportView(areaTweet);
 		scrollPane_4.setViewportView(listaTweets);
 		panelTwitter.setLayout(gl_panelTwitter);
