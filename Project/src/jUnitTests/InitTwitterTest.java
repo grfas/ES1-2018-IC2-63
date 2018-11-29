740c7dd8a1eaa95345fe75161ab6393d6ed9e9a4
gcanh <gcanh@172.17.5.153>
2018-11-29 16:13:59 +0000

    Tentativa de implementação de testes

--- Project/src/jUnitTests/InitTwitterTest.java
+++ Project/src/jUnitTests/InitTwitterTest.java
@@ -3,6 +3,8 @@ package jUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 import Twitter.TwitterApp;
@@ -13,7 +15,10 @@ class InitTwiterTest {
 	void test() {
 		TwitterApp junit = new TwitterApp();
 		assertNotNull(junit);
-	
-	}
 
+	}
+	PrintStream ConcatenarTokensAcesso( String accessToken, String accessTokenSecret) {
+		return System.out.printf( accessToken + accessTokenSecret);
+
+	}
 }
---
+++ Project/src/jUnitTests/TwitterTest.java
@@ -0,0 +1,14 @@
+package jUnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+//
+//class TwitterTest {
+//
+//	@Test
+//	void AdicionarTeste() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
